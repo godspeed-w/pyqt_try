e28e80183d6f9307068a42ff39ead63d73773658
godspeed-w <2399573003@qq.com>
2025-04-24 21:45:03 +0800

    完成单页面的简单测试

--- widgets/test_btn/test_btn.py
+++ widgets/test_btn/test_btn.py
@@ -1,10 +1,22 @@
-from widgets.test_but.test_btn import Ui_Form
-from PyQt5.QtWidgets import QWidget,QMessageBox
+import sys
+from test_btn_ui import Ui_Form
+# from widgets.test_btn.test_btn_ui import Ui_Form
+from PyQt5 import QtWidgets
 
-class Item(Ui_Form):
+class Item(QtWidgets.QWidget):
     def __init__(self):
-        super().__init__()
-        self.setupUi(self)
-        self.pushButton.clicked.connect(self.test_btn)
+        super(Item,self).__init__()
+        self.setWindowTitle("Item")
+        self.ui = Ui_Form()
+        self.ui.setupUi(self)
+
+        self.ui.pushButton.clicked.connect(self.test_btn)
+
     def test_btn(self):
         print("test_btn")
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    login_window = Item()
+    login_window.show()
+    sys.exit(app.exec_())

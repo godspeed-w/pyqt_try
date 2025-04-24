391838c3575adb0e1511ff1f6a0e4f0ffb5c1525
godspeed-w <2399573003@.qq.com>
2025-04-25 02:53:45 +0800

    自动扫描转码程序完成，项目初始化完成

--- README.md
+++ README.md
@@ -5,4 +5,7 @@ pyqt练习
 python3 、 pyqt5=5.15
 
 ## 实现功能
-计算器功能
+> 项目自动转换格式
+> > 使用python bin\autoSwitch.py 即可进行扫描转换
+
+* 项目框架搭建完成
--- bin/autoSwitch.py
+++ bin/autoSwitch.py
@@ -1,20 +1,68 @@
+"""自动检查目录下的ui文件是否变化, 如果有变化就自动转换为py文件"""
+
 import sys
 import os
+from pathlib import Path
+import time
 
 # DESIGNER_DIR = "C:\Users\wj\AppData\Local\Programs\Python\Python38\Lib\site-packages\qt5_applications\Qt\bin"
 # PYUIC_DIR = "C:\Users\wj\AppData\Local\Programs\Python\Python38\Scripts"
-PYUIC_DIR = ""
-OUT_DIR = ""
 
-pyui_file = os.path.join(PYUIC_DIR,"pyuic5.exe")
 
-# 构建命令
-command = f"{pyui_file} -o hello.py hello.ui"
-# 执行命令
-result = os.system(command)
+def scanFiles(dir, regex):
+    """扫描目录下的文件"""
+    files = {}
+    target_dir = Path(dir)
+    for file_path in target_dir.rglob(regex):
+        if file_path.is_file():
+            file_stat = file_path.stat()
+            # print(f"文件路径: {file_path}")
+            # print(f"文件大小: {file_stat.st_size} 字节")
+            # print(f"创建时间: {time.ctime(file_stat.st_ctime)}")
+            # print(f"修改时间: {time.ctime(file_stat.st_mtime)}")
+            # print(f"访问时间: {time.ctime(file_stat.st_atime)}")
+            updateTime = time.ctime(file_stat.st_mtime)
+            files[file_path.__str__()] = updateTime
+    return files
 
-if result == 0:
-    print("成功生成UI文件")
-else:
-    print("生成UI文件失败")
 
+def doTransform(file_path, pyui_file):
+    """执行转换"""
+    # 构建命令
+    directory = os.path.dirname(file_path)
+    # 获取文件名
+    filename = os.path.basename(file_path).split(".")[0]
+    pyFile = os.path.join(directory, filename + "_ui.py")
+    command = f"{pyui_file} -o {pyFile} {file_path}"
+    # 执行命令
+    result = os.system(command)
+    local_time = time.localtime(time.time())
+    formatted_time = time.strftime("%Y-%m-%d %H:%M:%S", local_time)
+    if result == 0:
+        print(formatted_time, "成功生成py文件:", command)
+    else:
+        print(formatted_time, "生成py文件失败:", command)
+
+
+if __name__ == "__main__":
+    PYUI_DIR = "widgets"
+    PYUIC_FILE = "pyuic5.exe"
+    REGEX = "*.ui"
+    # -----------------------------
+    allFiles = scanFiles(PYUI_DIR, REGEX)
+    print(allFiles.keys())
+    print("开始扫描...")
+    while True:
+        time.sleep(1)
+        tmpScanFiles = scanFiles(PYUI_DIR, REGEX)
+        for k, v in tmpScanFiles.items():
+            # 新增
+            if k not in allFiles.keys():
+                print("新增文件:", k)
+                allFiles[k] = v
+                doTransform(k, PYUIC_FILE)
+            # 修改
+            if k in allFiles.keys() and v != allFiles[k]:
+                print("修改文件:", k)
+                allFiles[k] = v
+                doTransform(k, PYUIC_FILE)

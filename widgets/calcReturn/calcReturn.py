5e06c432fbea65b4deb50319c1ee25752591c7a0
godspeed-w <2399573003@.qq.com>
2025-04-25 22:55:29 +0800

    完成画图

---
+++ widgets/calcReturn/calcReturn.py
@@ -0,0 +1,24 @@
+import sys
+from PyQt5 import QtWidgets
+try:
+    from widgets.calcReturn.calcReturn_ui import Ui_Form
+except:
+    from calcReturn_ui import Ui_Form
+
+class Item(QtWidgets.QWidget):
+    def __init__(self):
+        super(Item,self).__init__()
+        self.setWindowTitle("Item")
+        self.ui = Ui_Form()
+        self.ui.setupUi(self)
+
+        self.ui.pushButton.clicked.connect(self.test_btn)
+
+    def test_btn(self):
+        print("test_btn")
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    login_window = Item()
+    login_window.show()
+    sys.exit(app.exec_())
---
+++ widgets/calcReturn/calcReturn_ui.py
@@ -0,0 +1,283 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'calcReturn.ui'
+##
+## Created by: Qt User Interface Compiler version 5.15.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        if not Form.objectName():
+            Form.setObjectName(u"Form")
+        Form.resize(813, 515)
+        self.verticalLayout_3 = QVBoxLayout(Form)
+        self.verticalLayout_3.setObjectName(u"verticalLayout_3")
+        self.verticalLayout = QVBoxLayout()
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.horizontalLayout_11 = QHBoxLayout()
+        self.horizontalLayout_11.setObjectName(u"horizontalLayout_11")
+        self.horizontalLayout_4 = QHBoxLayout()
+        self.horizontalLayout_4.setObjectName(u"horizontalLayout_4")
+        self.label = QLabel(Form)
+        self.label.setObjectName(u"label")
+
+        self.horizontalLayout_4.addWidget(self.label)
+
+        self.lineEdit = QLineEdit(Form)
+        self.lineEdit.setObjectName(u"lineEdit")
+
+        self.horizontalLayout_4.addWidget(self.lineEdit)
+
+
+        self.horizontalLayout_11.addLayout(self.horizontalLayout_4)
+
+        self.horizontalLayout_5 = QHBoxLayout()
+        self.horizontalLayout_5.setObjectName(u"horizontalLayout_5")
+        self.label_2 = QLabel(Form)
+        self.label_2.setObjectName(u"label_2")
+
+        self.horizontalLayout_5.addWidget(self.label_2)
+
+        self.lineEdit_2 = QLineEdit(Form)
+        self.lineEdit_2.setObjectName(u"lineEdit_2")
+
+        self.horizontalLayout_5.addWidget(self.lineEdit_2)
+
+
+        self.horizontalLayout_11.addLayout(self.horizontalLayout_5)
+
+        self.horizontalLayout_2 = QHBoxLayout()
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.label_4 = QLabel(Form)
+        self.label_4.setObjectName(u"label_4")
+
+        self.horizontalLayout_2.addWidget(self.label_4)
+
+        self.dateEdit_2 = QDateEdit(Form)
+        self.dateEdit_2.setObjectName(u"dateEdit_2")
+
+        self.horizontalLayout_2.addWidget(self.dateEdit_2)
+
+
+        self.horizontalLayout_11.addLayout(self.horizontalLayout_2)
+
+
+        self.verticalLayout.addLayout(self.horizontalLayout_11)
+
+        self.horizontalLayout_12 = QHBoxLayout()
+        self.horizontalLayout_12.setObjectName(u"horizontalLayout_12")
+        self.horizontalLayout_6 = QHBoxLayout()
+        self.horizontalLayout_6.setObjectName(u"horizontalLayout_6")
+        self.label_6 = QLabel(Form)
+        self.label_6.setObjectName(u"label_6")
+
+        self.horizontalLayout_6.addWidget(self.label_6)
+
+        self.lineEdit_3 = QLineEdit(Form)
+        self.lineEdit_3.setObjectName(u"lineEdit_3")
+
+        self.horizontalLayout_6.addWidget(self.lineEdit_3)
+
+
+        self.horizontalLayout_12.addLayout(self.horizontalLayout_6)
+
+        self.horizontalLayout_7 = QHBoxLayout()
+        self.horizontalLayout_7.setObjectName(u"horizontalLayout_7")
+        self.label_5 = QLabel(Form)
+        self.label_5.setObjectName(u"label_5")
+
+        self.horizontalLayout_7.addWidget(self.label_5)
+
+        self.lineEdit_4 = QLineEdit(Form)
+        self.lineEdit_4.setObjectName(u"lineEdit_4")
+
+        self.horizontalLayout_7.addWidget(self.lineEdit_4)
+
+
+        self.horizontalLayout_12.addLayout(self.horizontalLayout_7)
+
+        self.horizontalLayout_3 = QHBoxLayout()
+        self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
+        self.label_8 = QLabel(Form)
+        self.label_8.setObjectName(u"label_8")
+
+        self.horizontalLayout_3.addWidget(self.label_8)
+
+        self.dateEdit_3 = QDateEdit(Form)
+        self.dateEdit_3.setObjectName(u"dateEdit_3")
+
+        self.horizontalLayout_3.addWidget(self.dateEdit_3)
+
+
+        self.horizontalLayout_12.addLayout(self.horizontalLayout_3)
+
+
+        self.verticalLayout.addLayout(self.horizontalLayout_12)
+
+        self.horizontalLayout_13 = QHBoxLayout()
+        self.horizontalLayout_13.setObjectName(u"horizontalLayout_13")
+        self.horizontalLayout_10 = QHBoxLayout()
+        self.horizontalLayout_10.setObjectName(u"horizontalLayout_10")
+        self.label_10 = QLabel(Form)
+        self.label_10.setObjectName(u"label_10")
+
+        self.horizontalLayout_10.addWidget(self.label_10)
+
+        self.lineEdit_7 = QLineEdit(Form)
+        self.lineEdit_7.setObjectName(u"lineEdit_7")
+        self.lineEdit_7.setEnabled(False)
+
+        self.horizontalLayout_10.addWidget(self.lineEdit_7)
+
+
+        self.horizontalLayout_13.addLayout(self.horizontalLayout_10)
+
+        self.horizontalLayout_9 = QHBoxLayout()
+        self.horizontalLayout_9.setObjectName(u"horizontalLayout_9")
+        self.label_9 = QLabel(Form)
+        self.label_9.setObjectName(u"label_9")
+
+        self.horizontalLayout_9.addWidget(self.label_9)
+
+        self.lineEdit_6 = QLineEdit(Form)
+        self.lineEdit_6.setObjectName(u"lineEdit_6")
+        self.lineEdit_6.setEnabled(False)
+
+        self.horizontalLayout_9.addWidget(self.lineEdit_6)
+
+
+        self.horizontalLayout_13.addLayout(self.horizontalLayout_9)
+
+        self.horizontalLayout_8 = QHBoxLayout()
+        self.horizontalLayout_8.setObjectName(u"horizontalLayout_8")
+        self.label_7 = QLabel(Form)
+        self.label_7.setObjectName(u"label_7")
+
+        self.horizontalLayout_8.addWidget(self.label_7)
+
+        self.lineEdit_5 = QLineEdit(Form)
+        self.lineEdit_5.setObjectName(u"lineEdit_5")
+        self.lineEdit_5.setEnabled(False)
+
+        self.horizontalLayout_8.addWidget(self.lineEdit_5)
+
+
+        self.horizontalLayout_13.addLayout(self.horizontalLayout_8)
+
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.label_3 = QLabel(Form)
+        self.label_3.setObjectName(u"label_3")
+
+        self.horizontalLayout.addWidget(self.label_3)
+
+        self.dateEdit = QDateEdit(Form)
+        self.dateEdit.setObjectName(u"dateEdit")
+
+        self.horizontalLayout.addWidget(self.dateEdit)
+
+
+        self.horizontalLayout_13.addLayout(self.horizontalLayout)
+
+
+        self.verticalLayout.addLayout(self.horizontalLayout_13)
+
+
+        self.verticalLayout_3.addLayout(self.verticalLayout)
+
+        self.verticalLayout_2 = QVBoxLayout()
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.groupBox = QGroupBox(Form)
+        self.groupBox.setObjectName(u"groupBox")
+        self.horizontalLayout_14 = QHBoxLayout(self.groupBox)
+        self.horizontalLayout_14.setObjectName(u"horizontalLayout_14")
+        self.tableWidget = QTableWidget(self.groupBox)
+        if (self.tableWidget.columnCount() < 8):
+            self.tableWidget.setColumnCount(8)
+        __qtablewidgetitem = QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(0, __qtablewidgetitem)
+        __qtablewidgetitem1 = QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(1, __qtablewidgetitem1)
+        __qtablewidgetitem2 = QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(2, __qtablewidgetitem2)
+        __qtablewidgetitem3 = QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(3, __qtablewidgetitem3)
+        __qtablewidgetitem4 = QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(4, __qtablewidgetitem4)
+        __qtablewidgetitem5 = QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(5, __qtablewidgetitem5)
+        __qtablewidgetitem6 = QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(6, __qtablewidgetitem6)
+        __qtablewidgetitem7 = QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(7, __qtablewidgetitem7)
+        self.tableWidget.setObjectName(u"tableWidget")
+
+        self.horizontalLayout_14.addWidget(self.tableWidget)
+
+
+        self.verticalLayout_2.addWidget(self.groupBox)
+
+        self.groupBox_2 = QGroupBox(Form)
+        self.groupBox_2.setObjectName(u"groupBox_2")
+        self.horizontalLayout_15 = QHBoxLayout(self.groupBox_2)
+        self.horizontalLayout_15.setObjectName(u"horizontalLayout_15")
+        self.widget = QWidget(self.groupBox_2)
+        self.widget.setObjectName(u"widget")
+
+        self.horizontalLayout_15.addWidget(self.widget)
+
+
+        self.verticalLayout_2.addWidget(self.groupBox_2)
+
+
+        self.verticalLayout_3.addLayout(self.verticalLayout_2)
+
+
+        self.retranslateUi(Form)
+
+        QMetaObject.connectSlotsByName(Form)
+    # setupUi
+
+    def retranslateUi(self, Form):
+        Form.setWindowTitle(QCoreApplication.translate("Form", u"Form", None))
+        self.label.setText(QCoreApplication.translate("Form", u"\u4ea7\u54c1\u540d\u79f0\uff1a", None))
+        self.label_2.setText(QCoreApplication.translate("Form", u"\u8d2d\u4e70\u6e20\u9053\uff1a", None))
+        self.label_4.setText(QCoreApplication.translate("Form", u"\u5230\u671f\u65e5\uff1a", None))
+        self.label_6.setText(QCoreApplication.translate("Form", u"\u5f53\u524d\u91d1\u989d\uff1a", None))
+        self.lineEdit_3.setText("")
+        self.label_5.setText(QCoreApplication.translate("Form", u"\u8d2d\u4e70\u91d1\u989d\uff1a", None))
+        self.lineEdit_4.setText("")
+        self.label_8.setText(QCoreApplication.translate("Form", u"\u8ba1\u7b97\u65e5\uff1a", None))
+        self.label_10.setText(QCoreApplication.translate("Form", u"\u5dee\u989d\uff1a", None))
+        self.lineEdit_7.setText("")
+        self.label_9.setText(QCoreApplication.translate("Form", u"\u8f83\u4e0a\u6b21\u5dee\u989d\uff1a", None))
+        self.lineEdit_6.setText("")
+        self.label_7.setText(QCoreApplication.translate("Form", u"\u5e74\u5316\uff1a", None))
+        self.lineEdit_5.setText("")
+        self.label_3.setText(QCoreApplication.translate("Form", u"\u8d2d\u4e70\u65e5\uff1a", None))
+        self.groupBox.setTitle(QCoreApplication.translate("Form", u"\u7406\u8d22\u8bb0\u5f55", None))
+        ___qtablewidgetitem = self.tableWidget.horizontalHeaderItem(0)
+        ___qtablewidgetitem.setText(QCoreApplication.translate("Form", u"\u7f16\u53f7", None));
+        ___qtablewidgetitem1 = self.tableWidget.horizontalHeaderItem(1)
+        ___qtablewidgetitem1.setText(QCoreApplication.translate("Form", u"\u4ea7\u54c1\u540d\u79f0", None));
+        ___qtablewidgetitem2 = self.tableWidget.horizontalHeaderItem(2)
+        ___qtablewidgetitem2.setText(QCoreApplication.translate("Form", u"\u6e20\u9053", None));
+        ___qtablewidgetitem3 = self.tableWidget.horizontalHeaderItem(3)
+        ___qtablewidgetitem3.setText(QCoreApplication.translate("Form", u"\u5f53\u524d\u91d1\u989d", None));
+        ___qtablewidgetitem4 = self.tableWidget.horizontalHeaderItem(4)
+        ___qtablewidgetitem4.setText(QCoreApplication.translate("Form", u"\u8d2d\u4e70\u91d1\u989d", None));
+        ___qtablewidgetitem5 = self.tableWidget.horizontalHeaderItem(5)
+        ___qtablewidgetitem5.setText(QCoreApplication.translate("Form", u"\u5dee\u989d", None));
+        ___qtablewidgetitem6 = self.tableWidget.horizontalHeaderItem(6)
+        ___qtablewidgetitem6.setText(QCoreApplication.translate("Form", u"\u5929\u6570", None));
+        ___qtablewidgetitem7 = self.tableWidget.horizontalHeaderItem(7)
+        ___qtablewidgetitem7.setText(QCoreApplication.translate("Form", u"\u6536\u76ca\u7387", None));
+        self.groupBox_2.setTitle(QCoreApplication.translate("Form", u"\u8d8b\u52bf\uff1a", None))
+    # retranslateUi
+

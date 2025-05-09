4747a54c94ce78f402aa1954aab499bfface23eb
godspeed-w <2399573003@qq.com>
2025-05-09 17:41:49 +0800

    完成利率计算，天数计算

--- README.md
+++ README.md
@@ -22,7 +22,7 @@ create table invest(
 	prd_status char(1) comment '产品状态（到期可赎回1、封闭期0）',
 	buy_amt float(10,2) comment '购买金额',
 	buy_date date comment '购买日',
-	due_date date comment '赎回日',
+	ma_date date comment '到期日',
 	ac_date date comment '会计日',
 	current_amt float(10,2) comment '记账日总金额',
 	profit float(10,2) comment '当前收益金额',
@@ -31,11 +31,11 @@ create table invest(
 	ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP comment '时间戳',
 	primary key (vest_id)
 )ENGINE=InnoDB  DEFAULT CHARSET=utf8 comment = '投资信息表';
-insert into invest (prd_channel, prd_name, prd_status, buy_date, due_date, ac_date, days, real_rate) values ('JNQLWQzpB0', 'XH3s', '1', '6067-07-10', '4799-03-02', '2196-11-10', 2920, 609);
-insert into invest (prd_channel, prd_name, prd_status, buy_date, due_date, ac_date, days, real_rate) values ('g', 'zNluHO1I', '1', '2984-03-21', '2949-02-19', '4366-02-01', 6425, 9411);
-insert into invest (prd_channel, prd_name, prd_status, buy_date, due_date, ac_date, days, real_rate) values ('i', 'FQ5yDzOxVu', '1', '8583-02-19', '2883-03-23', '3256-01-24', 5862, 4111);
-insert into invest (prd_channel, prd_name, prd_status, buy_date, due_date, ac_date, days, real_rate) values ('zQe', 'ACMNrJ6H', '0', '2485-02-26', '7920-10-03', '7525-02-10', 9657, 7138);
-insert into invest (prd_channel, prd_name, prd_status, buy_date, due_date, ac_date, days, real_rate) values ('CLwvTdm', '1SSWB7R', '0', '4261-03-01', '1399-10-14', '8574-06-20', 4513, 2544);
+insert into invest (prd_channel, prd_name, prd_status, buy_date, ma_date, ac_date, days, real_rate) values ('JNQLWQzpB0', 'XH3s', '1', '6067-07-10', '4799-03-02', '2196-11-10', 2920, 609);
+insert into invest (prd_channel, prd_name, prd_status, buy_date, ma_date, ac_date, days, real_rate) values ('g', 'zNluHO1I', '1', '2984-03-21', '2949-02-19', '4366-02-01', 6425, 9411);
+insert into invest (prd_channel, prd_name, prd_status, buy_date, ma_date, ac_date, days, real_rate) values ('i', 'FQ5yDzOxVu', '1', '8583-02-19', '2883-03-23', '3256-01-24', 5862, 4111);
+insert into invest (prd_channel, prd_name, prd_status, buy_date, ma_date, ac_date, days, real_rate) values ('zQe', 'ACMNrJ6H', '0', '2485-02-26', '7920-10-03', '7525-02-10', 9657, 7138);
+insert into invest (prd_channel, prd_name, prd_status, buy_date, ma_date, ac_date, days, real_rate) values ('CLwvTdm', '1SSWB7R', '0', '4261-03-01', '1399-10-14', '8574-06-20', 4513, 2544);
 select * from invest;
 
 ```
--- widgets/calcReturn/calcReturn.py
+++ widgets/calcReturn/calcReturn.py
@@ -18,7 +18,7 @@ class Invest_dbView():
         self.prd_status = str(data[4])
         self.buy_amt = str(data[5])
         self.buy_date = str(data[6])
-        self.due_date = str(data[7])
+        self.ma_date = str(data[7])
         self.ac_date = str(data[8])
         self.current_amt = str(data[9])
         self.profit = str(data[10])
@@ -35,13 +35,13 @@ class UiPageData():
         self.prd_channel = ""
         self.buy_amt = ""
         self.current_amt = ""
-        self.due_date = ""
+        self.ma_date = ""
         self.buy_date = ""
         self.ac_date = ""
+        self.days = ""
         self.prd_status = ""
         self.inqCondition_prdName = ""
         self.inqCondition_prdChannel = ""
-    
     
     
 class Item(QtWidgets.QWidget):
@@ -59,22 +59,37 @@ class Item(QtWidgets.QWidget):
             'database': 'book_keeping',
             'charset': 'utf8'
         }
-        self.ui.dateEdit_valDate.setDateTime(QDateTime.currentDateTime())
+        today = QDateTime.currentDateTime()
+        self.ui.dateEdit_acDate.setDateTime(today)
+        self.ui.dateEdit_buyDate.setDateTime(today)
+        # 事件绑定
         self.ui.pushButton_inquery.clicked.connect(self.doDbSearch)
         self.ui.pushButton_save.clicked.connect(self.doDbSave)
+        self.ui.dateEdit_buyDate.dateChanged.connect(self.calcDays)
+        self.ui.dateEdit_acDate.dateChanged.connect(self.calcDays)
+        # 计算收益与收益率
+        self.ui.lineEdit_cost.textChanged.connect(self.calcProfit)
+        self.ui.lineEdit_amt.textChanged.connect(self.calcProfit)
 
+    def is_number(self,s):
+        try:
+            float(s)
+            return True
+        except ValueError:
+            return False
+    
     def getUiDataVale(self):
         self.uiData.prd_name = self.ui.lineEdit_prdName.text().strip()
         self.uiData.prd_channel = self.ui.lineEdit_channel.text().strip()
         self.uiData.buy_amt = self.ui.lineEdit_cost.text().strip()
         self.uiData.current_amt = self.ui.lineEdit_amt.text().strip()
-        self.uiData.due_date = self.ui.dateEdit_maDate.date().toString("yyyy-MM-dd")
+        self.uiData.ma_date = self.ui.dateEdit_maDate.date().toString("yyyy-MM-dd")
         self.uiData.buy_date = self.ui.dateEdit_buyDate.date().toString("yyyy-MM-dd")
-        self.uiData.ac_date = self.ui.dateEdit_valDate.date().toString("yyyy-MM-dd")
+        self.uiData.ac_date = self.ui.dateEdit_acDate.date().toString("yyyy-MM-dd")
+        self.uiData.days = self.ui.lineEdit_days.text().strip()
         self.uiData.prd_status = self.ui.radioButton_statusY.text().strip()
         self.uiData.inqCondition_prdName = self.ui.lineEdit_search_condition.text().strip()
         self.uiData.inqCondition_prdChannel = self.ui.lineEdit_search_channel.text().strip()
-    
 
     def dbExcute(self, sql):
         try:
@@ -87,7 +102,6 @@ class Item(QtWidgets.QWidget):
         except Exception as e:
             print("执行失败:", e)
    
-    
     def dbSearch(self, sql):
         try:
             conn = pymysql.connect(**self.dbConfig)
@@ -117,11 +131,10 @@ class Item(QtWidgets.QWidget):
             self.ui.tableWidget.setItem(i, 6, QtWidgets.QTableWidgetItem(item.real_rate))
             self.ui.tableWidget.setItem(i, 7, QtWidgets.QTableWidgetItem(item.days))
             self.ui.tableWidget.setItem(i, 8, QtWidgets.QTableWidgetItem(item.buy_date))
-            self.ui.tableWidget.setItem(i, 9, QtWidgets.QTableWidgetItem(item.due_date))
+            self.ui.tableWidget.setItem(i, 9, QtWidgets.QTableWidgetItem(item.ma_date))
             self.ui.tableWidget.setItem(i, 10, QtWidgets.QTableWidgetItem(item.ac_date))
             self.ui.tableWidget.setItem(i, 11, QtWidgets.QTableWidgetItem(item.ts))
             self.ui.tableWidget.setItem(i, 12, QtWidgets.QTableWidgetItem(item.vest_id))
-
 
     def doDbSave(self):
         self.getUiDataVale()
@@ -136,18 +149,38 @@ class Item(QtWidgets.QWidget):
         real_rate = profit / float(self.uiData.buy_amt) * 100
         if len(data) > 0:
             formatResData = Invest_dbView(data[0])
-            updSql = "update invest set prd_channel='{}',prd_name='{}',prd_status='{}',buy_amt='{}',buy_date='{}',due_date='{}',ac_date='{}',current_amt='{}',profit='{}',days='{}',real_rate='{}' where vest_id='{}'"
-            updSql = updSql.format(self.uiData.prd_channel, self.uiData.prd_name, self.uiData.prd_status, self.uiData.buy_amt, self.uiData.buy_date, self.uiData.due_date, self.uiData.ac_date, self.uiData.current_amt, profit, days, real_rate, formatResData.vest_id) 
+            updSql = "update invest set prd_channel='{}',prd_name='{}',prd_status='{}',buy_amt='{}',buy_date='{}',ma_date='{}',ac_date='{}',current_amt='{}',profit='{}',days='{}',real_rate='{}' where vest_id='{}'"
+            updSql = updSql.format(self.uiData.prd_channel, self.uiData.prd_name, self.uiData.prd_status, self.uiData.buy_amt, self.uiData.buy_date, self.uiData.ma_date, self.uiData.ac_date, self.uiData.current_amt, profit, days, real_rate, formatResData.vest_id) 
             self.dbExcute(updSql)
             QtWidgets.QMessageBox.information(self, "提示", "更新成功")
         else:
-            insSql = "insert into invest (prd_channel,prd_name,prd_status,buy_amt,buy_date,due_date,ac_date,current_amt,profit,days,real_rate) values ('{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}')"
-            insSql = insSql.format(self.uiData.prd_channel, self.uiData.prd_name, self.uiData.prd_status, self.uiData.buy_amt, self.uiData.buy_date, self.uiData.due_date, self.uiData.ac_date, self.uiData.current_amt, profit, days, real_rate) 
+            insSql = "insert into invest (prd_channel,prd_name,prd_status,buy_amt,buy_date,ma_date,ac_date,current_amt,profit,days,real_rate) values ('{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}')"
+            insSql = insSql.format(self.uiData.prd_channel, self.uiData.prd_name, self.uiData.prd_status, self.uiData.buy_amt, self.uiData.buy_date, self.uiData.ma_date, self.uiData.ac_date, self.uiData.current_amt, profit, days, real_rate) 
             self.dbExcute(insSql)
             QtWidgets.QMessageBox.information(self, "提示", "保存成功")
-        
         self.doDbSearch()
-            
+    
+    def calcDays(self):
+        self.getUiDataVale()
+        print(self.uiData.buy_date, self.uiData.ac_date)
+        days = (datetime.datetime.strptime(self.uiData.ac_date, "%Y-%m-%d") - datetime.datetime.strptime(self.uiData.buy_date, "%Y-%m-%d")).days
+        self.ui.lineEdit_days.setText(str(days))
+        self.calcProfit()
+    
+    def calcProfit(self):
+        self.getUiDataVale()
+        if self.uiData.current_amt != "" and self.uiData.buy_amt != "":
+            if self.is_number(self.uiData.buy_amt) and self.is_number(self.uiData.current_amt):
+                current_amt = float(self.uiData.current_amt)
+                buy_amt = float(self.uiData.buy_amt)
+                days = int(self.uiData.days)
+                profit = current_amt - buy_amt
+                self.ui.lineEdit_profit.setText(str(profit))
+                if days == 0:
+                    self.ui.lineEdit_rate.setText("0")
+                    return
+                real_rate = round((profit / days * 365) / buy_amt,2)
+                self.ui.lineEdit_rate.setText(str(real_rate))
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
